993fe6c8ab9281799f44f22695b024bf89bdb10c
richa <richardnham2000@gmail.com>
2021-09-12 19:10:07 -0400

    Final Solution

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,21 @@
+package com.example;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 16 Solution
+ *  Copyright 2021 Richard Nham
+ */
+public class Main
+{
+    public static void main( String[] args ) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("What is your age? ");
+        int age = in.nextInt();
+
+        String out = (age >= 16)? "You are old enough to legally drive." : "You are not old enough to legally drive.";
+
+        System.out.print(out);
+    }
+}
